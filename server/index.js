22f286f8ea8b386f6ebb14975cd8b14bbea9a34d
Georgi Kirioukhine <georgi.kiriouk@gmail.com>
2023-03-30 21:30:09 -0400

    Final Project building blocks commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const express = require("express");
+
+const PORT = 8000;
+
+var app = express();
+
+app.use(express.json());
+
+app.get("/hello", (req, res) => {
+  res.status(200).json({ hi: "hi" });
+});
+
+const server = app.listen(PORT, function () {
+  console.info("🌍 Listening on port " + server.address().port);
+});
